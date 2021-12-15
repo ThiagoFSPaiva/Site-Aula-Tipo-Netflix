b0063ffece8580b1f6bbb74bd9172c0a8782943c
Thiago Paiva <thiagofellipe151@gmail.com>
2021-12-15 13:07:10 -0300

    Atualização Pre-Alpha 1.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Projeto Netflix de Aulas
 
-Versão beta 1.0
+Versão Pre-Alpha 1.0
 
 [Clique aqui para visualizar](https://thiagofspaiva.github.io/Site-Aula-Tipo-Netflix/)
 
---
+++ movierow.js
@@ -0,0 +1,11 @@
+import '/styles/style.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+
+<div classname="movierow">
+    <div classname="movierow--left">
+        <NavigateBeforeIcon style={{fontSize:50}}> </NavigateBeforeIcon>
+
+    </div>
+    
+</div>
